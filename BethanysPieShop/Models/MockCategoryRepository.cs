1ac6f7e864e5ccc8e5eb8b01b2f2b3f35ae35bc3
nhatnhanchiha <68037847+nhatnhanchiha@users.noreply.github.com>
2020-12-10 12:19:36 +0700

    First

---
+++ BethanysPieShop/Models/MockCategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace BethanysPieShop.Models
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> AllCategories =>
+            new List<Category>
+            {
+                new Category
+                {
+                    CategoryId = 1, CategoryName = "Fruit pies", Description = "All fruit"
+                }
+            };
+    }
+}
---
+++ BethanysPieShop/Models/MockPieRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace BethanysPieShop.Models
+{
+    public class MockPieRepository : IPieRepository
+    {
+        public IEnumerable<Pie> AllPies { get; }
+        public IEnumerable<Pie> PiesOfTheWeek { get; }
+        public Pie GetById(int pieId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
